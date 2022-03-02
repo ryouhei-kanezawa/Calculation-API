01741ea4a8f4e035dfe4f2656ef5797c174ebaa2
ryouhei-kanezawa <k.ryouhei1414@gmail.com>
2022-03-02 15:52:02 +0900

    途中

--- app/Http/Controllers/CalculationFormula.php
+++ app/Http/Controllers/CalculationFormula.php
@@ -8,8 +8,15 @@ class CalculationFormula extends Controller
 {
     public function squared(Request $request){
         $tmp = $request->input('number');
+        $tortal = $tmp * $tmp;
+        
+        $link = mysqli_connect('localhost', 'root', '', 'network');
+        $stmt = mysqli_prepare($link, "INSERT INTO results (name, result) VALUES (?, ?)");
+        mysqli_stmt_bind_param($stmt, "sd", "squared", $tortal);
+        mysqli_stmt_execute($stmt);
+        mysqli_close($link);
 
-        return $tmp * $tmp;
+        return $tortal;
     }
 
     public function cube(Request $request){
